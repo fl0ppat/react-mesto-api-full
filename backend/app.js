76c2f6ca762d9ef6d123af99dd60af6246190997
Daniel Khrenov <klaven011@gmail.com>
2021-05-20 11:30:32 +0300

    feat: add rateLimiter and helmet

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const { isCelebrateError } = require('celebrate');
 const cors = require('cors');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+
 const NotFoundError = require('./errors/NotFound');
 const handleError = require('./middlewares/errors');
 const BadRequestError = require('./errors/BadRequest');
@@ -11,6 +14,10 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 
 const app = express();
+const limit = rateLimiter({
+  windowMs: 600000, // 10 min
+  max: 100,
+});
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -20,10 +27,16 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(express.json());
+
+app.use(helmet());
+app.disable('x-powered-by');
 app.use(cors({
   credentials: true,
   origin: 'https://fl0ppat.nomoredomains.club',
 }));
+
+app.use(limit);
+
 app.use(requestLogger);
 app.get('/crash-test', () => {
   setTimeout(() => {
